8880031c0f193a79c1305c4a539eb516b1dc3aab
OD1995 <oliverdernie1@googlemail.com>
2021-06-04 15:11:11 +0100

    fully working, I think

--- EventGridTrigger/__init__.py
+++ EventGridTrigger/__init__.py
@@ -8,29 +8,24 @@ import azure.durable_functions as df
 async def main(event: func.EventGridEvent,
             starter: str):
 
-    client = df.DurableOrchestrationClient(starter)
 
-    ## File path (from event.subject) takes below form
-    ##    "/blobServices/default/containers/{CONTAINER_NAME}/blobs/{BLOB_NAME}"
-    ## Blob name (file name) will include folder if necessary
+	fileURL = event.get_json()['url']
+	container = fileURL.split("/")[3]
+	logging.info(f"fileURL: {fileURL}")
+	logging.info(f"container: {container}")
 
-    subject = (event.subject).split('/')
-    result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
-    options = {
-        "fileUrl": event.get_json()['url'],
-        "container": subject[4],
-        "blob": subject[6]
-	}
+	if fileURL.endswith(".mp4") & (container == "audiotranscript-files"):
+		
+		client = df.DurableOrchestrationClient(starter)
 
-    logging.info("starter----------------> %s",options)
-    logging.info("result----------------> %s",result)
+		instance_id = await client.start_new(
+			orchestration_function_name="Orchestrator",
+			instance_id=None,
+			client_input={
+				'fileURL' : fileURL
+			}
+		)
+		logging.info(f"https://fseaudiotranscription.azurewebsites.net/runtime/webhooks/durabletask/instances/{instance_id}?taskHub=DurableFunctionsHub&connection=Storage&code=Xo4jdHQLMhoihVlz7Rjc9RmX6ElkOx3q7ySHnKMXQ1KM9/Zam1mGmw==")
 
-#     instance_id = await client.start_new(orchestration_function_name="Orchestrator",
-#                                             instance_id=None,
-#                                             client_input=options)
+	else:
+		logging.info("not MP4 or not `audiotranscript-files` so no processing done")
